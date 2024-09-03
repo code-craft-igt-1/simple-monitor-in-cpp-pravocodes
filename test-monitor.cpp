aae5cce97ff0a93f75565735e44ef0b80199c520
Praveen Kumar <114364779+pravocodes@users.noreply.github.com>
2024-09-03 22:57:35 +0530

    test cases updated

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,6 +3,8 @@
 #include "gtest/gtest.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
+  ASSERT_FALSE(vitalsOk(99, 102, 98));
+  ASSERT_FALSE(vitalsOk(103, 70, 98));
+  ASSERT_FALSE(vitalsOk(98, 99, 89));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
